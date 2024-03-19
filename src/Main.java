cdd156ee140bb8e0d9b76d57ff712f36600db5c1
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-03-19 11:01:45 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TreeSet<Book> set = new TreeSet<>();
+
+        set.add(new Book(1, "Beyaz Kalp", "Javier Marias", 250, 2016));
+        set.add(new Book(2, "Siddhartha", "Herman Hesse", 150, 2002));
+        set.add(new Book(3, "Walden Gölü", "Henry David Thoreau", 380, 2017));
+        set.add(new Book(4, "Dorian Gray'in Portresi", "Oscar Wilde", 275, 2002));
+        set.add(new Book(5, "Cehenneme Övgü", "Gündüz Vassaf", 275, 2000));
+
+        for (Book book : set) {
+            System.out.println(book.getID() + "--" + book.getBookName() + "-- " +
+                    book.getAuthorName() + "--" + book.getPageNumber() + "--" + book.getPublishDate());
+        }
+
+        System.out.println("#####################################");
+
+        TreeSet<Book> set2 = new TreeSet<>(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getPageNumber() - o2.getPageNumber();
+            }
+        });
+
+        set2.add(new Book(1, "Beyaz Kalp", "Javier Marias", 250, 2016));
+        set2.add(new Book(2, "Siddhartha", "Herman Hesse", 150, 2002));
+        set2.add(new Book(3, "Walden Gölü", "Henry David Thoreau", 380, 2017));
+        set2.add(new Book(4, "Dorian Gray'in Portresi", "Oscar Wilde", 275, 2002));
+        set2.add(new Book(5, "Cehenneme Övgü", "Gündüz Vassaf", 275, 2000));
+
+        for (Book book : set2) {
+            System.out.println(book.getID() + "--" + book.getBookName() + "-- " +
+                    book.getAuthorName() + "--" + book.getPageNumber() + "--" + book.getPublishDate());
+        }
     }
 }
+
